7f7566034f2988becf5ba4f3eb0d4f6927b9d51d
Павел Менчуков <midiy009@mail.ru>
2018-09-30 23:28:27 +0300

    Added function `P2P-lib.log(message : str)`.

--- P2P-lib/P2P_lib.py
+++ P2P-lib/P2P_lib.py
@@ -1,1 +1,22 @@
+#Encoding : utf-8
 
+import asyncio
+
+console_sync = asyncio.Lock()
+
+async def log(message : str, mode : str = "server", file_only : bool = False):
+    import time
+    await console_sync.acquire()
+    str_time = time.asctime()
+    str_message = f"[{str_time}] : {message}"
+    if not message.endswith("."):
+        str_message += "."
+    if not file_only:
+        print(str_message)
+    try:
+        log_file = open(f"{mode}.log", "at")
+        log_file.write(str_message + "\n")
+        log_file.close()
+    except Exception as e:
+        print(e)
+    console_sync.release()
