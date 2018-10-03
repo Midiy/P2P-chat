5c5b577e817136c7ed9a1492342b37ad4db9ef9a
Павел Менчуков <midiy009@mail.ru>
2018-10-04 00:10:39 +0300

    Added documentation to `P2P_lib.log()`.
    Also names of "private" functions now started with "_".

--- P2P-lib/P2P_lib.py
+++ P2P-lib/P2P_lib.py
@@ -1,6 +1,15 @@
 #Encoding : utf-8
 
 def log(message : str, mode : str = "server", file_only : bool = False):
+    """
+    Logging message to console and file.
+
+    message - Message to logging.
+    mode - Log file will be named "<mode>.log"; default is "server".
+    file_only - if True, message will not output to console; default is False.
+
+    Log string is "[<time.asctime()>] : <message>".
+    """
     from time import asctime
     str_time = asctime()
     str_message = f"[{str_time}] : {message}"
--- P2P-server/P2P_server.py
+++ P2P-server/P2P_server.py
@@ -4,29 +4,29 @@ import asyncio
 import concurrent.futures
 from P2P_lib import log
 
-async def get_data(reader : asyncio.StreamReader, timeout : int = 10) -> (int, bytes):
-    async def _get_data(len : int):
+async def _get_data(reader : asyncio.StreamReader, timeout : int = 10) -> (int, bytes):
+    async def _l_get_data(len : int):
         data = await asyncio.wait_for(reader.read(len), timeout=timeout)
         if data:
             return data
         else:
             raise ConnectionAbortedError()
-    len = await _get_data(1)
-    result = await _get_data(int(len[0]))
+    len = await _l_get_data(1)
+    result = await _l_get_data(int(len[0]))
     return (int(result[0]), result[1:])
 
-async def send_data(writer : asyncio.StreamReader, code : int, data : bytes):
+async def _send_data(writer : asyncio.StreamReader, code : int, data : bytes):
     data_to_send = bytes([len(data) + 1, code]);
     data_to_send += data
     await writer.write(data_to_send)
     await writer.drain()
 
-async def handle_connection(reader : asyncio.StreamReader, writer : asyncio.StreamWriter):
+async def _handle_connection(reader : asyncio.StreamReader, writer : asyncio.StreamWriter):
     peername = writer.get_extra_info("peername")
     log(f"Accepted connection from {peername}.")
     while True:
         try:
-            code, data = await get_data(reader)
+            code, data = await _get_data(reader)
             # TODO: Reaction on recieved data.
         except ConnectionAbortedError:
             log(f"Connection from {peername} closed by peer.")
@@ -36,17 +36,18 @@ async def handle_connection(reader : asyncio.StreamReader, writer : asyncio.Stre
             break
     writer.close()
 
-async def wait_for_interrupt():
+async def _wait_for_interrupt():
     while True:
         await asyncio.sleep(5)
     
 if __name__ == "__main__":
+    log("\n")
     loop = asyncio.get_event_loop()
-    server_gen = asyncio.start_server(handle_connection, port=3501)
+    server_gen = asyncio.start_server(_handle_connection, port=3501)
     server = loop.run_until_complete(server_gen)
     # It seems like KeyboardInterrupt handled somewhere in
     # server() coroutine, then I have to add following line.
-    loop.create_task(wait_for_interrupt())
+    loop.create_task(_wait_for_interrupt())
     log(f"Listening established on {server.sockets[0].getsockname()}.")
     try:
         loop.run_forever()
